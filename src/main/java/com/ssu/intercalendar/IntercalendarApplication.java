b2e139aa290b890918ba418f74007e15445653dd
SH Park <psh01053803870@gmail.com>
2024-05-15 16:13:04 +0900

    Initial commit

---
+++ src/main/java/com/ssu/intercalendar/IntercalendarApplication.java
@@ -0,0 +1,13 @@
+package com.ssu.intercalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssu/intercalendar/IntercalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssu.intercalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
